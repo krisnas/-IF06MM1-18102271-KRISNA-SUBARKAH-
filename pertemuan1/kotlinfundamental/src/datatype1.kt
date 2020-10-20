d1d634efdc83205cb236eae333bda3aea5af0ba1
krisnas <krisnasubarkah@gmail.com>
2020-10-20 07:10:12 +0700

    create project assignment for meet1

---
+++ pertemuan1/kotlinfundamental/src/datatype1.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var kampus: String = " IT Telkom"
+    kampus = " IT Telkom Purwokerto"
+    val jurusan: String = "Teknik"
+    jurusan = "Teknik Informatika" // Val cannot be reassigned
+}
