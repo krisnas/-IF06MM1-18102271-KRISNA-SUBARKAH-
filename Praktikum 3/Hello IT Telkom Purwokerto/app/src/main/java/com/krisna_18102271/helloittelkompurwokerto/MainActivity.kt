2cbd9bd5d247b40ed30b624468b51e31bbb4794d
ashasembiring <73183992+ashasembiring@users.noreply.github.com>
2020-10-26 19:29:14 +0700

    Pertemuan 3

---
+++ Praktikum 3/Hello IT Telkom Purwokerto/app/src/main/java/com/krisna_18102271/helloittelkompurwokerto/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.krisna_18102271.helloittelkompurwokerto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
