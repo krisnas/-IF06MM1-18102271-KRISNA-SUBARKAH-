0a266aa6dc8e14417bfc416917aca9da380f8938
krisnas <krisnasubarkah@gmail.com>
2020-11-21 11:25:54 +0700

    I've done my assignment on Practice-7, please check, Thanks Sir

--- pertemuan1/kotlinfundamental/src/datatype2.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val kataAwal = "Saya"
-    val kataAkhir = "Hebat"
-    val nilaiA: Int = 10
-    val nilaiB = 20
-    print(nilaiA + nilaiB)
-    print("\n")
-    print(kataAwal + kataAkhir)
-}
--- pertemuan1/kotlinfundamental/src/whileexpressions2.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    var counter = 1
-    do {
-        println("Hello, World!")
-        counter++
-    } while (counter <= 7)
-}
