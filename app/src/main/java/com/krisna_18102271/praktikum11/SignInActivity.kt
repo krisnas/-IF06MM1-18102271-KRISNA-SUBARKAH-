c61de65e2be6df60c6b09f8ce2ebc7b08574c9e2
krisnas <krisnasubarkah@gmail.com>
2021-01-12 12:05:40 +0700

    Firebase, Membuat kelas Sign In dan Sign Up

---
+++ app/src/main/java/com/krisna_18102271/praktikum11/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.krisna_18102271.praktikum11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
