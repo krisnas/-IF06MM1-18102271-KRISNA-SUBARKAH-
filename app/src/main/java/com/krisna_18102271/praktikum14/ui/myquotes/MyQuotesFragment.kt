8f6f1363be2b39e09cf24dd965279eb4d0e41770
krisnas <krisnasubarkah@gmail.com>
2021-02-03 13:06:02 +0700

    init prak 14 dan persiapan UI

---
+++ app/src/main/java/com/krisna_18102271/praktikum14/ui/myquotes/MyQuotesFragment.kt
@@ -0,0 +1,31 @@
+package com.krisna_18102271.praktikum14.ui.myquotes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.krisna_18102271.praktikum14.R
+
+class MyQuotesFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProvider(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_my_quotes, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
