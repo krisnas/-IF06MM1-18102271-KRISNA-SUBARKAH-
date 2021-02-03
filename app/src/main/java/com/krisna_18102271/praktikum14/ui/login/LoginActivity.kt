b10443b35596c52a9c1636df3c373212770d8c7d
krisnas <krisnasubarkah@gmail.com>
2021-02-03 13:10:11 +0700

    membuat package login dan activitynya

---
+++ app/src/main/java/com/krisna_18102271/praktikum14/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.krisna_18102271.praktikum14.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.krisna_18102271.praktikum14.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
